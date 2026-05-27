ed12880912cc82d7077bceb024b622628f5c18be
Taylor Stein <taylor@engagedmd.com>
2026-05-27 15:52:27 -0400

    chore: gitignore .astro/ build cache
    Astro generates this directory on dev/build; shouldn't ship with the repo.

--- .astro/content-modules.mjs
+++
@@ -1,8 +0,0 @@
-
-export default new Map([
-["src/content/strategy/01-vision.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fstrategy%2F01-vision.mdx&astroContentModuleFlag=true")],
-["src/content/strategy/02-who-ai-serves.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fstrategy%2F02-who-ai-serves.mdx&astroContentModuleFlag=true")],
-["src/content/strategy/03-differentiators.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fstrategy%2F03-differentiators.mdx&astroContentModuleFlag=true")],
-["src/content/strategy/05-execution.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fstrategy%2F05-execution.mdx&astroContentModuleFlag=true")],
-["src/content/strategy/04-foundations.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fstrategy%2F04-foundations.mdx&astroContentModuleFlag=true")]]);
-		
